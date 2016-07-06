da562802240f60754a70765108cf7ee5a7b30cb2
88sanjay <88.sanjay@gmail.com>
2016-07-06 17:38:33 -0400

    scala changes

--- src/main/scala/analysis.scala
+++ src/main/scala/analysis.scala
@@ -1,4 +1,5 @@
-import org.apache.spark.{SparkContext,SparkConf}
+import org.apache.spark.{SparkContext,SparkConf,SQLContext}
+import org.apache.spark.sql.SQLContext.implicits._
 import scala.collection.mutable.{Map => MutableMap}
 
 object Spark {
@@ -56,100 +57,92 @@ object LogParser extends java.io.Serializable {
     }
 }
 
-object FeatureExtraction {
-
+object FeatureExtraction extends java.io.Serializable {
     def getPath(url:String): String = {
         var pattern = """\.[a-zA-Z0-9]+:?[0-9]*(\/[a-zA-Z0-9]+)\/""".r
         var elems = pattern.findFirstMatchIn(url)
         if (elems == None ) {
             pattern = """\.[a-zA-Z0-9]+:?[0-9]*(\/[a-zA-Z0-9]*)""".r
-            elems = pattern.findFirstInm.(url)
+            elems = pattern.findFirstMatchIn(url)
         }
         return (if (elems !=  None) elems.get.group(1) else "/")
     }
+    // '''
+    // getSessionTime -  method to get time duration of a particular session
+    // '''
+    def getSessionTime(groupedMaps:Array[Map[String,String]]): Double = {
+        var ret = 0.0
+        if (groupedMaps.length > 1) {
 
-// '''
-// getSessionTime -  method to get time duration of a particular session
-// '''
-    // def getSessionTime(x):
-    //     ret = 0
-    //     if len(x[1]) > 1:
-    //         delta = x[1][-1]["timestamp"] - x[1][0]["timestamp"]
-    //         ret = delta.total_seconds()
-    //     return (ret + max(0.0,x[1][-1]["request_processing_time"]) + \
-    //     max(0.0,x[1][-1]["backend_processing_time"]) + \
-    //     max(0.0,x[1][-1]["response_processing_time"]))
+            ret = (LogParser.timeFormat.parse(groupedMaps.last("timestamp")).getTime() - 
+            LogParser.timeFormat.parse(groupedMaps(0)("timestamp")).getTime())/1000
+        }
+        return (ret + math.max(0.0,groupedMaps.last("request_processing_time").toDouble) + 
+        math.max(0.0,(groupedMaps.last("backend_processing_time").toDouble)) +
+        math.max(0.0,(groupedMaps.last("response_processing_time").toDouble)))
+    }
+    // '''
+    // countKPIs  - method to count number of occurances of each element of y 
+    //              in x
+    // '''
+    def countKPIs(x:Array[String],y:Array[String],name:String): Array[(String,Double)] = {
+        var ret = Array[(String,Double)]()
+        for (i <- y) ret = ret :+ (name+"_"+i.replace('.','*'),x.count(_ == i)*1.0)
+        return ret
+    }
+    // '''
+    // createKPIs  - method to create all features
+    // '''
+    val elbstatus = Array("1","2","3","4","5")
 
-// '''
-// countKPIs  - method to count number of occurances of each element of y 
-//              in x
-// '''
+    val hostlist = Array("/favicon", "/api", "", "/papi", "/offer", "/shop")
 
-// def countKPIs(x,y,name):
-//     ret = []
-//     for i in y:
-//         ret = ret+[(name+"_"+i.replace('.','*'),x.count(i))]
-//     return ret
-
-// '''
-// createKPIs  - method to create all features
-// '''
-
-// def createKPIs(x,formapper=True):
-//     # Session time
-//     sessiontime = [("sessiontime",getSessionTime(x))]
-//     # Unique URLs
-//     numurls = [("numurls",len(set([i['request'] for i in x[1]])))]
-//     # Time per url
-//     timeperurl = [("timeperurl",sessiontime[0][1]/numurls[0][1])]
-//     avgdeltatimes = [("avgdeltatimes",sum(getDeltas(x[1]))/len(x[1]))]
-//     # backend ip
-//     backendips = countKPIs([i["backend:port"].split(":")[0] for i in x[1]],
-//         bipbc.value,"backendip")
-//     avg_request_processing_time = [("avg_request_processing_time_per_url",
-//         sum([max(0,i['request_processing_time']) for i in x[1]])/numurls[0][1])]
-//     avg_backend_processing_time = [("avg_backend_processing_time_per_url",
-//         sum([max(0,i['backend_processing_time']) for i in x[1]])/numurls[0][1])]
-//     avg_response_processing_time = [("avg_response_processing_time_per_url",
-//         sum([max(0,i['response_processing_time']) for i in x[1]])/numurls[0][1])]
-//     elbstatuscode = countKPIs([i["elb_status_code"][0] for i in x[1]],
-//         elbstatusbc.value,"elb_status_code")    
-//     total_received_bytes = [
-//     ("total_received_bytes_per_url",
-//         sum([float(i['received_bytes']) for i in x[1]])/numurls[0][1])
-//     ]
-//     total_sent_bytes = [
-//     ("total_sent_bytes_per_url",
-//         sum([float(i['sent_bytes']) for i in x[1]])/numurls[0][1])
-//     ]
-//     request = countKPIs([getPath(i["request"]) for i in x[1]],
-//         hostbc.value,"request")
-//     # user_agent
-//     ssl_protocol = countKPIs([i["ssl_protocol"] for i in x[1]],sslprotbc.value,"sslport")
-//     ret = dict(sessiontime+
-//         numurls+
-//         request+
-//         backendips+
-//         elbstatuscode+
-//         avg_response_processing_time+
-//         avg_backend_processing_time+
-//         avg_request_processing_time+
-//         timeperurl+
-//         avgdeltatimes+
-//         total_received_bytes+
-//         total_sent_bytes+
-//         ssl_protocol)
-//     if formapper:
-//         return ret.values()
-//     return ret.keys() 
-
+    def createKPIs(groupedMaps:Array[Map[String,String]],formapper:Boolean = true) : Map[String,Double]={
+        // Session time
+        val sessiontime:(String,Double) = ("sessiontime",getSessionTime(groupedMaps))
+        // Unique URLs
+        val numurls:(String,Double) = ("numurls",groupedMaps.map(i=> i("request") ).distinct.length)
+        // Time per url
+        val timeperurl:(String,Double) = ("timeperurl",sessiontime._2/numurls._2)
+        val avgdeltatimes:(String,Double) = ("avgdeltatimes",LogParser.getDeltas(groupedMaps).sum/groupedMaps.length)
+        val avg_request_processing_time :(String,Double) =  ("avg_request_processing_time_per_url",
+            groupedMaps.map(i=> math.max(0.0,i("request_processing_time").toDouble)).sum/numurls._2)
+        val avg_backend_processing_time :(String,Double) =  ("avg_backend_processing_time_per_url",
+            groupedMaps.map(i=> math.max(0.0,i("backend_processing_time").toDouble)).sum/numurls._2)
+        val avg_response_processing_time :(String,Double) =  ("avg_response_processing_time_per_url",
+            groupedMaps.map(i=> math.max(0.0,i("response_processing_time").toDouble)).sum/numurls._2)
+        val total_received_bytes :(String,Double) = 
+        ("total_received_bytes_per_url",
+            groupedMaps.map(i=> i("received_bytes").toDouble).sum/numurls._2)
+        val total_sent_bytes :(String,Double) = 
+        ("total_sent_bytes_per_url",
+            groupedMaps.map(i=> i("sent_bytes").toDouble).sum/numurls._2)
+        val elbstatuscode:Array[(String,Double)] = countKPIs(groupedMaps.map(i=> i("elb_status_code")),
+            elbstatus,"elb_status_code")    
+        val request :Array[(String,Double)] = countKPIs(
+            groupedMaps.map(i=> getPath(i("request"))),hostlist,"request")
+        var ret = Array[(String,Double)]()
+        ret = ret :+ sessiontime :+
+            numurls :+
+            avg_response_processing_time :+
+            avg_backend_processing_time :+ 
+            avg_request_processing_time :+
+            timeperurl :+
+            avgdeltatimes :+
+            total_received_bytes :+
+            total_sent_bytes
+        ret = ret ++
+            request ++ 
+            elbstatuscode
+        return ret.toMap
+    }
 }
 
 object analysis {
     def main(args: Array[String]) {
         val dataHome = "/home/sanjay/Kidagiri/algorithm_ds/paytm/WeblogChallenge/data/"
         val dataPath = dataHome + "*.log"
-        val rawRDD = sc.textFile(dataPath).map(LogParser.parser).cache()
+        val rawRDD = Spark.sc.textFile(dataPath).map(LogParser.parser).cache()
         val ipGroupedAnalysisRDD = rawRDD.map(
             x => {(x("client:port").split(':')(0) , List(x))}
             ).reduceByKey(
@@ -168,5 +161,9 @@ object analysis {
             LogParser.splitOnSessionDelta(x._1,x._2)
             )
 
+        val temprecord = sessionAnalysisRDD.take(1)(0)._2
+        val KPIs = sessionAnalysisRDD.map(i => FeatureExtraction.createKPIs(i._2))
+        val LowSessionTimeUsers = KPIs.filter(i=> i("sessiontime")  < 350)
+        val HighSessionTimeUsers = KPIs.filter(i=> i("sessiontime")  >= 350)
     }
 }
